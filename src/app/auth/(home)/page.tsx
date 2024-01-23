b387ea2a2c1591200884530b776952ab51155f6f
Claudio Lins <claudio.lins@me.com>
2024-01-23 14:49:57 +0000

    core: upgrade Nextjs

--- src/app/auth/(home)/page.tsx
+++ src/app/auth/(home)/page.tsx
@@ -2,7 +2,7 @@ export default async function AuthPage() {
   await new Promise((resolve) => setTimeout(resolve, 1000));
   return (
     <div className="flex h-screen w-full items-center justify-center">
-      AuthPage
+      <h1>AuthPage</h1>
     </div>
   );
 }
