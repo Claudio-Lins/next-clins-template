7f361a012c4ac29a93d74467408cc651cf9115eb
Claudio Lins <claudio.lins@me.com>
2024-02-09 19:32:27 +0000

    fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
   images: {
     domains: ["images.unsplash.com", "res.cloudinary.com"],
   },
--- src/app/auth/(home)/page.tsx
+++
@@ -1,8 +0,0 @@
-export default async function AuthPage() {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  return (
-    <div className="flex h-screen w-full items-center justify-center">
-      <h1>AuthPage</h1>
-    </div>
-  );
-}
